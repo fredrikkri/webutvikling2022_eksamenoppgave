93b34a724e1a6c8c89a981e795d38d1b38d9881c
fredrikkri <96488359+fredrikkri@users.noreply.github.com>
2023-10-01 20:59:03 +0200

    Add files via upload

---
+++ javascript/ham.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("meny");
+
+const hamMenu = () =>{
+    if(menu.style.display == "block"){
+        menu.style.display = "none";
+    }
+    else{
+        menu.style.display = "block";
+    }
+}
+// Jeg har tatt med denne javascript filen for å gjøre hamburger-meny tigjengelig.
